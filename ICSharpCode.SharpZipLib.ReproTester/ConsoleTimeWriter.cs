947cc452fce46db3c39a7509212f182e466d22e9
nils måsén <nils@piksel.se>
2018-06-16 19:49:37 +0200

    Update repro framework

--- ICSharpCode.SharpZipLib.ReproTester/ConsoleTimeWriter.cs
+++ ICSharpCode.SharpZipLib.ReproTester/ConsoleTimeWriter.cs
@@ -9,13 +9,17 @@ namespace ICSharpCode.SharpZipLib.ReproTester
 {
 	class ConsoleTimeWriter : TextWriter
 	{
-		const int BUFFER_SIZE = 26;
+		const int BUFFER_SIZE = 32;
+		const int BUFFER_MAX = BUFFER_SIZE - 1;
 
 		static Stream stdOut;
 		static ConsoleTimeWriter instance;
 		static byte[] buffer = new byte[BUFFER_SIZE];
 
 		static bool trailing = false;
+
+		const string PREFIX_STRING = "[yyyy-MM-dd HH:mm:ss.fff] ";
+		public const int PREFIX_LENGTH = 26; // prefixString.Length;
 
 		public static ConsoleTimeWriter Instance
 			=> instance ?? (instance = new ConsoleTimeWriter());
@@ -37,6 +41,9 @@ namespace ICSharpCode.SharpZipLib.ReproTester
 		{
 		}
 
+		public override void WriteLine()
+			=> Write("", true);
+
 		public override void WriteLine(string value)
 			=> Write(value, true);
 
@@ -53,7 +60,7 @@ namespace ICSharpCode.SharpZipLib.ReproTester
 			{
 				var currCol = Console.ForegroundColor;
 				Console.ForegroundColor = ConsoleColor.Gray;
-				var timestamp = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss.fff] ");
+				var timestamp = DateTime.Now.ToString(PREFIX_STRING);
 				count = Console.OutputEncoding.GetBytes(timestamp, 0, timestamp.Length, buffer, 0);
 				stdOut.Write(buffer, 0, count);
 				Console.ForegroundColor = currCol;
@@ -61,11 +68,29 @@ namespace ICSharpCode.SharpZipLib.ReproTester
 
 			do
 			{
-				count = (unread - index);
+				if (index > unread) break;
 
-				count = Console.OutputEncoding.GetBytes(value, index, count > BUFFER_SIZE ? BUFFER_SIZE : count, buffer, 0);
-				if (newline && count < (BUFFER_SIZE-1))
-					buffer[count++] = (byte)'\n';
+				else if (index < unread)
+				{
+					count = (unread - index);
+					count = Console.OutputEncoding.GetBytes(value, index, count > BUFFER_SIZE ? BUFFER_SIZE : count, buffer, 0);
+				}
+				else
+				{
+					count = 0;
+				}
+				if (newline)
+				{
+					if (count < BUFFER_SIZE)
+					{
+						buffer[count++] = (byte)'\n';
+					}
+					else if (count == BUFFER_SIZE && (index + count) == unread)
+					{
+	
+					}
+				}
+
 				stdOut.Write(buffer, 0, count);
 				index += count;
 
@@ -93,5 +118,116 @@ namespace ICSharpCode.SharpZipLib.ReproTester
 			stdOut.Write(cbuf, 0, Console.OutputEncoding.GetBytes(new[] { value }, 0, 1, cbuf, 0));
 		}
 #endif
+
+	}
+
+	public class ConsoleWriter
+	{
+		private static ConsoleWriter defaultInstance = new ConsoleWriter();
+
+		private ConsoleWriter() { }
+
+		public int LinePrefixLength { get; set; }
+
+		private int getIndentation(int indentation)
+			=> indentation > 0 ? indentation : LinePrefixLength + indentation;
+
+		public static ConsoleWriter Write(object o, ConsoleColor color, int indentation, bool indentFirst = false)
+		{
+			var cc = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			var cw = Write(o, indentation, indentFirst);
+			Console.ForegroundColor = cc;
+			return cw;
+		}
+
+		public static ConsoleWriter Write(object o, ConsoleColor color)
+		{
+			var cc = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			Write(o);
+			Console.ForegroundColor = cc;
+			return defaultInstance;
+		}
+
+		internal static PrefixedConsoleWriter Prefixed(string prefix, ConsoleColor color)
+		{
+			return new PrefixedConsoleWriter(prefix, color);
+		}
+
+		public static ConsoleWriter Write(object o)
+		{
+			Console.Write(o);
+			return defaultInstance;
+		}
+
+		public static ConsoleWriter Write(object o, int indentation, bool indentFirst = false)
+		{
+			var cw = new ConsoleWriter();
+			cw.LinePrefixLength = indentation;
+			var indentString = getIndentString(indentation);
+			var indentedSep = "\n" + indentString;
+
+			if (indentFirst)
+				Console.Write(indentString);
+
+			Console.Write(o.ToString().Replace("\n", indentedSep));
+
+			return cw;
+		}
+
+		private static string getIndentString(int indentation)
+			=> new string(' ', indentation);
+
+		public ConsoleWriter _(object o, int indentation)
+			=> Write(o, getIndentation(indentation));
+
+		public ConsoleWriter _(object o)
+			=> Write(o);
+
+		public ConsoleWriter _(object o, ConsoleColor color, int indentation)
+			=> Write(o, color, getIndentation(indentation));
+
+		public ConsoleWriter _(object o, ConsoleColor color)
+			=> Write(o, color);
+
+		public void End(ConsoleColor? color = null)
+		{
+			if (color.HasValue)
+				Console.ForegroundColor = color.Value;
+
+			Console.WriteLine();
+		}
+	}
+
+	internal class PrefixedConsoleWriter
+	{
+		private readonly string prefix;
+		private readonly ConsoleColor prefixColor;
+		private readonly int prefixLength;
+
+		public PrefixedConsoleWriter(string prefix, ConsoleColor prefixColor)
+		{
+			this.prefix = prefix;
+			this.prefixColor = prefixColor;
+			this.prefixLength = prefix.Length;
+		}
+
+        //public int LinePrefixLength { get; set; }
+
+        private int getIndentation(int indentation)
+			=> indentation + prefixLength;
+
+		public ConsoleWriter Write(object o, ConsoleColor color)
+			=> ConsoleWriter.Write(prefix, prefixColor)._(o, color, 0);
+
+		public ConsoleWriter Write(object o, ConsoleColor color, int indentation, bool indentFirst = false)
+			=> ConsoleWriter.Write(prefix, prefixColor, getIndentation(indentation), indentFirst)._(o, color, 0);
+
+		public ConsoleWriter Write(object o)
+			=> ConsoleWriter.Write(prefix, prefixColor)._(o);
+
+		public ConsoleWriter Write(object o, int indentation, bool indentFirst = false)
+			=> ConsoleWriter.Write(prefix, prefixColor, getIndentation(indentation), indentFirst)._(o, 0);
 	}
 }
--- ICSharpCode.SharpZipLib.ReproTester/Program.cs
+++ ICSharpCode.SharpZipLib.ReproTester/Program.cs
@@ -1,75 +1,177 @@
 ﻿using ICSharpCode.SharpZipLib.ReproTester.Repros;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ICSharpCode.SharpZipLib.ReproTester
 {
 	static class Program
 	{
+		private static PrefixedConsoleWriter mainLogger;
+		private static string workRoot;
+
+		public static string WorkRoot => workRoot;
+
 		static void Main()
 		{
-			
 			Console.Title = "SharpZipLib Repro Tester";
+			Console.ForegroundColor = ConsoleColor.White;
 			Console.WriteLine(Console.Title);
 			Console.WriteLine();
 
+			// Hook the console so that timestamps and color can be added
 			ConsoleTimeWriter.HookConsole();
+
+			// Hook the Debug/Trace outputs (does not work on NETCORE)
 			ConsoleTimeWriter.HookTrace();
 
-			Run<Repro218>();
+			mainLogger = ConsoleWriter.Prefixed("[Main] ", ConsoleColor.Yellow);
+
+			workRoot = Path.Combine(Path.GetTempPath(), "SharpZipRepro");
+			if(Directory.Exists(workRoot))
+			{
+				mainLogger.Write("Cleaning work directory from previous tests... ");
+				Directory.Delete(workRoot, true);
+				mainLogger.Write("OK").End();
+			}
+
+			Directory.CreateDirectory(workRoot);
+
+			Run<Repro229>();
+			//Run<Repro218>();
 			//Run<Repro213>();
 			//Run<Repro204>();
 			//Run<Repro118>();
 			//Run<Repro208>();
 
+			//RunAll();
+
 			Console.Write("Press any key to exit... ");
 			Console.ReadKey();
 		}
 
-		private static void Run<TRepro>() where TRepro: Repro, new()
+		private static void RunAll()
 		{
-			var name = typeof(TRepro).Name;
-			TRepro repro = null;
+			mainLogger.Write($"Warning: Running all repros in current assembly.", ConsoleColor.Yellow).End();
+
+			int succeeded = 0;
+			int failed = 0;
+			foreach (var et in Assembly.GetCallingAssembly().DefinedTypes)
+			{
+				if(et != typeof(ReproTEMPLATE) && et != typeof(Repro) && typeof(Repro).IsAssignableFrom(et))
+				{
+					mainLogger.Write("Start of ")._(et.Name, ConsoleColor.Cyan).End();
+
+					var timeOut = et.GetCustomAttribute<TimeoutAttribute>()?.TimeOut 
+						?? TimeSpan.FromMinutes(3);
+
+					timeOut = TimeSpan.FromSeconds(10);
+
+					var result = Run(et.Name, () => et.DeclaredConstructors.First().Invoke(new object[0]) as Repro, timeOut);
+					if (result)
+						succeeded++;
+					else
+						failed++;
+
+					mainLogger.Write("End of ")._(et.Name, ConsoleColor.Cyan)._("\n").End();
+				}
+			}
+
+			ConsoleWriter.Write($"Out of ")
+				._(failed + succeeded, ConsoleColor.Cyan)
+				._(" repro(s), ")
+				._(failed, ConsoleColor.Red)
+				._(" failed and ")
+				._(succeeded, ConsoleColor.Green)
+				._(" ran without exceptions.").End();
+		}
+
+		private static bool Run<TRepro>() where TRepro : Repro, new()
+			=> Run(typeof(TRepro).Name, () => new TRepro());
+
+		private static bool Run(string name, Func<Repro> constructor, TimeSpan? timeOut = null)
+		{
+			if (!timeOut.HasValue) timeOut = TimeSpan.FromMinutes(5);
+
+			Repro repro = null;
+			Exception threadException = null;
+
+			var reproLog = ConsoleWriter.Prefixed($"[{name}] ", ConsoleColor.White);
+			//reproLog.LinePrefixLength = ConsoleTimeWriter.PREFIX_LENGTH;
+
 			try
 			{
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine($"[{name}] Preparing...");
-				Console.ForegroundColor = ConsoleColor.Gray;
-				repro = new TRepro();
+				reproLog.Write("Preparing...", ConsoleColor.Cyan).End(ConsoleColor.Gray);
 
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine($"[{name}] Running...");
-				Console.ForegroundColor = ConsoleColor.White;
-				repro.Run();
+				repro = constructor();
 
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine($"[{name}] No unhandled exceptions were caught.");
+				var t = new Thread((ro) =>
+				{
+					try
+					{
+						if (ro is Repro r)
+						{
+							r.Run();
+						}
+						else
+						{
+							threadException = new InvalidOperationException("Invalid parameter passed to repro runner thread");
+						}
+					}
+					catch (Exception x)
+					{
+						threadException = x;
+					}
+				});
+
+				reproLog.Write("Running...", ConsoleColor.Cyan).End(ConsoleColor.White);
+
+				t.Start(repro);
+				if (!t.Join(timeOut.Value))
+				{
+					t.Abort();
+					if (!t.Join(TimeSpan.FromSeconds(20)))
+						reproLog.Write("Warning: The repro thread did not abort correctly, cleanup may fail!", ConsoleColor.Yellow).End();
+
+					throw new TimeoutException($@"Timed out waiting for repro to finish ({timeOut:mm\m\ ss\s})");
+				}
+
+				if (threadException != null)
+					throw threadException;
+
+				reproLog.Write("No unhandled exceptions were caught.", ConsoleColor.Green).End();
+				return true;
 			}
 			catch (Exception x)
 			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"[{name}] {x.GetType().Name} thrown: {x.Message}");
+				var xw = reproLog.Write($"{x.GetType().Name} thrown.\nMessage: {x.Message}", ConsoleColor.Red, ConsoleTimeWriter.PREFIX_LENGTH);
+				xw._($"\n{x.StackTrace}", ConsoleColor.Red, 0);
+
 				var ix = x.InnerException;
-				while (ix != null) {
-					Console.WriteLine($"  {ix.GetType().Name}: {ix.Message}");
+				int level = 0;
+				while (ix != null)
+				{
+					level += 2;
+					xw._($"\n{ix.GetType().FullName}: {ix.Message}", ConsoleColor.Red, level);
 					ix = ix.InnerException;
 				}
-				Console.WriteLine("Stacktrace:");
-				Console.WriteLine(x.StackTrace);
-				Console.WriteLine();
+				xw.End();
+
+				return false;
 			}
 			finally
 			{
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine($"[{name}] Cleaning up...");
-				Console.ForegroundColor = ConsoleColor.White;
+				reproLog.Write("Cleaning up...", ConsoleColor.Cyan).End();
 				repro?.Cleanup();
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine($"[{name}] Done!\n");
+				reproLog.Write("Done!", ConsoleColor.Cyan).End();
 			}
 		}
+
+
+
 	}
 }
